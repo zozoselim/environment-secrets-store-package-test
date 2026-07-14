472aa64813b84a6ded6d0ddcb23866b0e9f808b7
Zulfia Nazar Gul Saleem Gul <zulfiyeselim05@gmail.com>
2026-07-14 11:37:08 +0300

    Fix package installation configuration

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
 
+
 setuptools.setup(
     name="environment-secrets-store",
     version="0.0.1",
@@ -7,25 +8,22 @@ setuptools.setup(
     author_email="info@novavision.ai",
     description="Environment Secrets Store component for NovaVision",
     url="https://github.com/zozoselim/environment-secrets-store-package-test",
-    license='MIT',
-    install_requires=['sdk', 'opencv-python-headless'],
-
+    license="MIT",
+    install_requires=[
+        "sdk",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-
     packages=[
-        'novavision.package',
-        'novavision.package.classes',
-        'novavision.package.configs',
-        'novavision.package.dataloaders',
-        'novavision.package.executors',
-        'novavision.package.models',
-        'novavision.package.utils',
-        'novavision.package.weights'
+        "novavision.package",
+        "novavision.package.executors",
+        "novavision.package.models",
+        "novavision.package.utils",
     ],
-    package_dir={'novavision.package': 'src'},
-    python_requires=">=3.6"
+    package_dir={
+        "novavision.package": "src",
+    },
+    python_requires=">=3.6",
 )
